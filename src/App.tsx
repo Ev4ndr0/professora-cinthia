9ea4df334781c59c9fbfc81a035d03cf7c223d22
Evandro Macedo <evandro.macedo5@gmail.com>
2024-11-15 16:51:52 -0300

    feat: add menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,20 @@
-import { Github, Wand2 } from 'lucide-react';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import {
+  Github,
+  Home,
+  LogOut,
+  Package,
+  Settings2,
+  ShoppingBag,
+  Users,
+  Wand2
+} from 'lucide-react';
+import Link from "next/link";
 import { useState } from "react";
 import { Button } from "./components/ui/button";
 import { Input } from './components/ui/input';
@@ -32,70 +48,178 @@ export function App() {
   }
 
   return (
-    <div className="min-h-screen flex flex-col">
-      <div className="px-6 py-3 flex items-center justify-between border-b">
-        <h1 className="text-xl font-bold">Calculo Frequência Média da Classe</h1>
+    <>
+      <div className="flex w-full flex-col bg-muted/40">
+        <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 border-r bg-background sm:flex flex-col">
+          <nav className="flex flex-col items-center gap-4 px-2 py-5">
+            <TooltipProvider>
+              <Link
+                href="#"
+                className="flex h-9 w-9 shrink-0 items-center justify-center bg-primary
+                        text-primary-foreground rounded-full"
+              >
+                <Package className="h-4 w-4" />
+                <span className="sr-only">Dashboard Avatar</span>
+              </Link>
 
-        <div className="flex items-center gap-3">
-          <span className="text-sm text-muted-foreground">
-            Desenvolvido por Evandro Macedo
-          </span>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <Link
+                    href="#"
+                    className="flex h-9 w-9 shrink-0 items-center justify-center rounded-lg
+                        text-muted-foreground transition-colors hover:text-foreground"
+                  >
+                    <Home className="h-5 w-5" />
+                    <span className="sr-only">Inicio</span>
+                  </Link>
+                </TooltipTrigger>
+                <TooltipContent side="right">Inicio</TooltipContent>
+              </Tooltip>
 
-          <Separator orientation="vertical" className="h-6" />
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <Link
+                    href="#"
+                    className="flex h-9 w-9 shrink-0 items-center justify-center rounded-lg
+                        text-muted-foreground transition-colors hover:text-foreground"
+                  >
+                    <ShoppingBag className="h-5 w-5" />
+                    <span className="sr-only">Pedido</span>
+                  </Link>
+                </TooltipTrigger>
+                <TooltipContent side="right">Pedido</TooltipContent>
+              </Tooltip>
 
-          <Button variant="outline">
-            <Github className="w-4 h-4 mr-2" />
-            GitHub
-          </Button>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <Link
+                    href="#"
+                    className="flex h-9 w-9 shrink-0 items-center justify-center rounded-lg
+                        text-muted-foreground transition-colors hover:text-foreground"
+                  >
+                    <Package className="h-5 w-5" />
+                    <span className="sr-only">Produto</span>
+                  </Link>
+                </TooltipTrigger>
+                <TooltipContent side="right">Produto</TooltipContent>
+              </Tooltip>
+
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <Link
+                    href="#"
+                    className="flex h-9 w-9 shrink-0 items-center justify-center rounded-lg
+                        text-muted-foreground transition-colors hover:text-foreground"
+                  >
+                    <Users className="h-5 w-5" />
+                    <span className="sr-only">Clientes</span>
+                  </Link>
+                </TooltipTrigger>
+                <TooltipContent side="right">Clientes</TooltipContent>
+              </Tooltip>
+
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <Link
+                    href="#"
+                    className="flex h-9 w-9 shrink-0 items-center justify-center rounded-lg
+              text-muted-foreground transition-colors hover:text-foreground"
+                  >
+                    <Settings2 className="h-5 w-5" />
+                    <span className="sr-only">Configurações</span>
+                  </Link>
+                </TooltipTrigger>
+                <TooltipContent side="right">Configurações</TooltipContent>
+              </Tooltip>
+
+            </TooltipProvider>
+          </nav>
+
+          <nav className="mt-auto flex flex-col items-center gap-4 px-2 py-5">
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <Link
+                    href="#"
+                    className="flex h-9 w-9 shrink-0 items-center justify-center rounded-lg
+              text-muted-foreground transition-colors hover:text-foreground"
+                  >
+                    <LogOut className="h-5 w-5 text-red-500" />
+                    <span className="sr-only">Sair</span>
+                  </Link>
+                </TooltipTrigger>
+                <TooltipContent side="right">Sair</TooltipContent>
+              </Tooltip>
+
+            </TooltipProvider>
+
+          </nav>
+        </aside>
+        <div className="sm: ml-14 px-6 py-3 flex items-center justify-between border-b">
+          <h1 className="text-xl font-bold">Calculo Frequência Média da Classe</h1>
+
+          <div className="flex items-center gap-3">
+            <span className="text-sm text-muted-foreground">
+              Desenvolvido por Evandro Macedo
+            </span>
+
+            <Separator orientation="vertical" className="h-6" />
+
+            <Button variant="outline">
+              <Github className="w-4 h-4 mr-2" />
+              GitHub
+            </Button>
+          </div>
         </div>
       </div>
+      <div className="min-h-screen flex flex-col">
+        <main className="sm: ml-14 flex-1 p-6 flex gap-6">
+          <aside className="w-full space-y-2">
+            <form className="space-y-6">
+              <div className="flex flex-col flex-1 gap-4">
+                <div className="grid grid-rows-4 gap-4 flex-1">
+                  <div className="grid w-full max-w-sm items-center gap-2">
+                    <div className="space-y-1">
+                      <Label>Quantidade de crianças</Label>
+                      <Input type="number" id="qtd_criancas" placeholder="Quantidade de crianças"
+                        onChange={handleCriancas} min={1} max={40} />
+                    </div>
+                    <Separator />
+                    <div className="space-y-1">
+                      <Label>Quantidade de Dias Letivos</Label>
+                      <Input type="number" id="nr_dias_letivos" placeholder="Quantidade de Dias Letivos"
+                        onChange={handleDias} min={1} max={40} />
+                    </div>
+                    <Separator />
+                    <div className="space-y-1">
+                      <Label>Quantidade total de faltas</Label>
+                      <Input type="number" id="qtd_total_faltas" placeholder="Quantidade total de faltas"
+                        onChange={handleFaltas} min={1} max={365} />
+                    </div>
+                  </div>
+                  <div className="space-y-1">
+                    <Button onClick={handleCalculoFrequencia} type="button" className="w-100">
+                      Calcular
+                      <Wand2 className="w-4 h-4 ml-2" />
+                    </Button>
+                  </div>
+                </div>
 
-      <main className="flex-1 p-6 flex gap-6">
-        <aside className="w-full space-y-2">
-          <form className="space-y-6">
-            <div className="flex flex-col flex-1 gap-4">
-              <div className="grid grid-rows-4 gap-4 flex-1">
-                <div className="grid w-full max-w-sm items-center gap-2">
-                  <div className="space-y-1">
-                    <Label>Quantidade de crianças</Label>
-                    <Input type="number" id="qtd_criancas" placeholder="Quantidade de crianças"
-                      onChange={handleCriancas} min={1} max={40} />
-                  </div>
-                  <Separator />
-                  <div className="space-y-1">
-                    <Label>Quantidade de Dias Letivos</Label>
-                    <Input type="number" id="nr_dias_letivos" placeholder="Quantidade de Dias Letivos"
-                      onChange={handleDias} min={1} max={40} />
-                  </div>
-                  <Separator />
-                  <div className="space-y-1">
-                    <Label>Quantidade total de faltas</Label>
-                    <Input type="number" id="qtd_total_faltas" placeholder="Quantidade total de faltas"
-                      onChange={handleFaltas} min={1} max={365} />
-                  </div>
-                </div>
-                <div className="space-y-1">
-                  <Button onClick={handleCalculoFrequencia} type="button" className="w-100">
-                    Calcular
-                    <Wand2 className="w-4 h-4 ml-2" />
-                  </Button>
-                </div>
               </div>
-
+            </form>
+          </aside>
+          <aside className="w-full space-y-2">
+            <div className="space-y-2">
+              <h1 className="text-xl font-bold">
+                A Frequência Média da Classe é <span className='text-blue-700'>{frequenciaMedia} </span>%
+              </h1>
             </div>
-          </form>
-        </aside>
-        <aside className="w-full space-y-2">
-          <div className="space-y-2">
-            <h1 className="text-xl font-bold">
-              A Frequência Média da Classe é <span className='text-blue-700'>{frequenciaMedia} </span>%
-            </h1>
+          </aside>
+          <div className="w-30 h-4 mr-2" >
+            <img src={logo} alt='professoa' />
           </div>
-        </aside>
-        <div className="w-30 h-4 mr-2" >
-          <img src={logo} alt='professoa'/>
-        </div>
-      </main>
-    </div>
+        </main>
+      </div>
+    </>
   )
 }
---
+++ src/login/page.tsx
@@ -0,0 +1,10 @@
+const LoginPage = async () => {
+
+    return (
+        <div>
+            <h1>Login</h1>
+        </div>
+    )
+};
+
+export default LoginPage;
